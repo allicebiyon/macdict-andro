897669fc7b1fc47be05adeb8e212c5db0c27b2eb
Allice <ericafontaine@gmail.com>
2017-01-19 05:56:02 -0500

    Update macdict-andro.py

--- macdict-andro.py
+++ macdict-andro.py
@@ -24,38 +24,42 @@ except ET.ParseError:
     print "入力ファイルを正しくパースできませんでした。XMLファイルではない可能性があります。"
     sys.exit(2)
 
-fout_name = fin.name.replace(".plist", "") + '_android.txt'
-fout = codecs.open(fout_name, 'w', 'utf-8')
-for array in root:
-    for dict_entry in array:
-        phrase_flag = False
-        shortcut_flag = False
-        phrase = ""
-        shortcut = ""
-        
-        for child in dict_entry:
-
-            if child.tag == "key" and child.text == "phrase":
-                phrase_flag = True
-                shortcut_flag = False
-                continue
+try:
+    fout_name = fin.name.replace(".plist", "") + '_android.txt'
+    fout = codecs.open(fout_name, 'w', 'utf-8')
+    for array in root:
+        for dict_entry in array:
+            phrase_flag = False
+            shortcut_flag = False
+            phrase = ""
+            shortcut = ""
             
-            if phrase_flag:
-                phrase_flag = False
-                phrase = child.text
+            for child in dict_entry:
 
-            if child.tag == "key" and child.text == "shortcut":
-                shortcut_flag = True
-                phrase_flag = False
-                continue
+                if child.tag == "key" and child.text == "phrase":
+                    phrase_flag = True
+                    shortcut_flag = False
+                    continue
+                
+                if phrase_flag:
+                    phrase_flag = False
+                    phrase = child.text
 
-            if shortcut_flag:
-                shortcut_flag = False
-                shortcut = child.text
+                if child.tag == "key" and child.text == "shortcut":
+                    shortcut_flag = True
+                    phrase_flag = False
+                    continue
 
-        #print shortcut + "\t" + phrase + "\t" + "asdf"
-        fout.write(shortcut + "\t" + phrase + "\t" + u"名詞" + "\n")
+                if shortcut_flag:
+                    shortcut_flag = False
+                    shortcut = child.text
 
-fout.close()
+            #print shortcut + "\t" + phrase + "\t" + "asdf"
+            fout.write(shortcut + "\t" + phrase + "\t" + u"名詞" + "\n")
 
-print fout_name + " が書き出されました。"
+    fout.close()
+    print fout_name + " が書き出されました。"
+
+except:
+    print "なんかよく分からないけど失敗しました！"
+    sys.exit(1)
