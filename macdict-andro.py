7706e7d174efb693f268a7dd0742af7e21108e82
allicebiyon <ericafontaine@gmail.com>
2017-01-19 05:35:00 -0500

    Update macdict-andro.py

--- macdict-andro.py
+++ macdict-andro.py
@@ -18,43 +18,43 @@ except IOError, msg:
     parser.error(str(msg))
 
 try:
-	tree = ET.parse(fin)
-	root = tree.getroot()
+    tree = ET.parse(fin)
+    root = tree.getroot()
 except ET.ParseError:
-	print "入力ファイルを正しくパースできませんでした。XMLファイルではない可能性があります。"
-	sys.exit(2)
+    print "入力ファイルを正しくパースできませんでした。XMLファイルではない可能性があります。"
+    sys.exit(2)
 
 fout_name = fin.name.replace(".plist", "") + '_android.txt'
 fout = codecs.open(fout_name, 'w', 'utf-8')
 for array in root:
-	for dict_entry in array:
-		phrase_flag = False
-		shortcut_flag = False
-		phrase = ""
-		shortcut = ""
-		
-		for child in dict_entry:
+    for dict_entry in array:
+        phrase_flag = False
+        shortcut_flag = False
+        phrase = ""
+        shortcut = ""
+        
+        for child in dict_entry:
 
-			if child.tag == "key" and child.text == "phrase":
-				phrase_flag = True
-				shortcut_flag = False
-				continue
-			
-			if phrase_flag:
-				phrase_flag = False
-				phrase = child.text
+            if child.tag == "key" and child.text == "phrase":
+                phrase_flag = True
+                shortcut_flag = False
+                continue
+            
+            if phrase_flag:
+                phrase_flag = False
+                phrase = child.text
 
-			if child.tag == "key" and child.text == "shortcut":
-				shortcut_flag = True
-				phrase_flag = False
-				continue
+            if child.tag == "key" and child.text == "shortcut":
+                shortcut_flag = True
+                phrase_flag = False
+                continue
 
-			if shortcut_flag:
-				shortcut_flag = False
-				shortcut = child.text
+            if shortcut_flag:
+                shortcut_flag = False
+                shortcut = child.text
 
-		#print shortcut + "\t" + phrase + "\t" + "asdf"
-		fout.write(shortcut + "\t" + phrase + "\t" + u"名詞" + "\n")
+        #print shortcut + "\t" + phrase + "\t" + "asdf"
+        fout.write(shortcut + "\t" + phrase + "\t" + u"名詞" + "\n")
 
 fout.close()
 
